9c168ed9f82e315c6a6a0ea6f7932780297ef4ab
Nivetha-K01 <121307499+Nivetha-K01@users.noreply.github.com>
2026-09-04 12:35:26 +0530

    Data -> AppDbContext

---
+++ AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using PJAverageRate.Models;
+
+namespace PJAverageRate.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+
+           : base(options) { }
+
+        public DbSet<PJAverageRateViewModel> PJAverageRateVal { get; set; }
+
+        public DbSet<GoldRateModel> GoldRates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GoldRateModel>()
+                .HasNoKey();
+            modelBuilder.Entity<PJAverageRateViewModel>()
+                .HasNoKey();
+
+        }
+
+
+    }
+
+
+}
