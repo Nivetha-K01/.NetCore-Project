33c09489f38033b49f53f1e7bf16afc3d8372f58
Nivetha-K01 <121307499+Nivetha-K01@users.noreply.github.com>
2026-09-04 12:33:36 +0530

    common pick folder

---
+++ PickListModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PJAverageRate.Models.CommonPick
+{
+    public class PickListModel
+    {
+        public List<string> Columns { get; set; }
+        public List<List<string>> Rows { get; set; }
+    }
+}
+    
