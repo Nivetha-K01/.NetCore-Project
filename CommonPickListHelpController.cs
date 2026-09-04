7950491b94a34418cb54dfa22f8a1f2f02e90b76
Nivetha-K01 <121307499+Nivetha-K01@users.noreply.github.com>
2026-09-04 12:36:01 +0530

    conttroller->picklist

---
+++ CommonPickListHelpController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PJAverageRate.Repository;
+namespace PJAverageRate.PickListController
+{
+    public class CommonPickListHelpController : Controller
+    {
+        private readonly ICommonPickRepository _repository;
+
+        public CommonPickListHelpController(ICommonPickRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public IActionResult OpenHelp(string helpCode, string query)
+        {
+            var data = _repository.GetPickList(query);
+
+            return PartialView("_CommonPick", data);
+        }
+    }
+}
